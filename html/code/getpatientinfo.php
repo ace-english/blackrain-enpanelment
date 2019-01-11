73e98a3e1b894d93177949a4a326c89b4bfb2151
kvang0200 <kvang0900@gmail.com>
2019-01-11 02:10:41 -0800

    Updated SideMenus and Database page.

--- html/code/getpatientinfo.php
+++ html/code/getpatientinfo.php
@@ -1,46 +1,93 @@
-<?php 
-require_once('connect.php');
+<html>
+<head>
+	<title>Database Check</title>
+	<link rel="stylesheet" href="../stylesheets/style.css">
+	<link rel="stylesheet" href="../stylesheets/kaiser.css">
+	<link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+</head>
+<body>
 
-$query = "SELECT PatientID, Fname, Mname, Lname, DOB, Gender, Sex, Language, Race, Ethnicity FROM patient";
+	<table class="patient-info-header">
+		<tr>
+			<td><img src="../img/menu.png" align="middle" href="index.html" onclick="openNav()"&#9776; >
+				CT# - <span id="ct-num"> 1234567890</span>
+			</td><td>
+				<h1 id="patient-name">Database</h1>
+			</td><td>
+				<img src="../img/bell.png" align="middle" >
+				USER ID
+				<span id="user-id">_________</span>
+			</tr>
+		</table>
 
-$responce = @mysqli_query($dbc, $query);
+		<div id="mySidenav" class="sidenav">
+			<a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+			<a href="../login.html">Log In</a>
+			<a href="../index.html">Home</a>
+			<a href="searchPatient.php">Search Patients</a>
+			<a href="#">Contact</a>
+		</div>
 
-if($responce){
-	echo'<table align = "left"
-	cellspaceing="5" cellpadding="8" border="1">
-	<tr><td align="left"><b>PatientID</b></td>
-	<td align="left"><b>First Name</b></td>
-	<td align="left"><b>Middle Name</b></td>
-	<td align="left"><b>Last Name</b></td>
-	<td align="left"><b>DOB</b></td>
-	<td align="left"><b>Gender</b></td>
-	<td align="left"><b>Sex</b></td>
-	<td align="left"><b>Language</b></td>
-	<td align="left"><b>Race</b></td>
-	<td align="left"><b>Ethnicity</b></td></tr>';
+		<div id="floating-box">
+			<?php 
+			require_once('connect.php');
 
-	while($row = mysqli_fetch_array($responce)){
+			$query = "SELECT PatientID, Fname, Mname, Lname, DOB, Gender, Sex, Language, Race, Ethnicity FROM patient";
 
-		echo '<tr><td align="left">' .
-		$row['PatientID'] . '</td><td align="left">' .
-		$row['Fname'] . '</td><td align="left">' .
-		$row['Mname'] . '</td><td align="left">' .
-		$row['Lname'] . '</td><td align="left">' .
-		$row['DOB'] . '</td><td align="left">' .
-		$row['Gender'] . '</td><td align="left">' .
-		$row['Sex'] . '</td><td align="left">' .
-		$row['Language'] . '</td><td align="left">' .
-		$row['Race'] . '</td><td align="left">'.
-		$row['Ethnicity'] . '</td><td align="left">';
-		echo '</tr>';
-	}
+			$responce = @mysqli_query($dbc, $query);
 
-	echo'</table>';
-} else{
-	echo "Couldn't issue database query<br />";
-	echo mysqli_error($dbc);
-}
+			if($responce){
+				echo'<table align = "left"
+				cellspaceing="5" cellpadding="8" border="1">
+				<tr><td align="left"><b>PatientID</b></td>
+				<td align="left"><b>First Name</b></td>
+				<td align="left"><b>Middle Name</b></td>
+				<td align="left"><b>Last Name</b></td>
+				<td align="left"><b>DOB</b></td>
+				<td align="left"><b>Gender</b></td>
+				<td align="left"><b>Sex</b></td>
+				<td align="left"><b>Language</b></td>
+				<td align="left"><b>Race</b></td>
+				<td align="left"><b>Ethnicity</b></td></tr>';
 
-mysqli_close($dbc);
+				while($row = mysqli_fetch_array($responce)){
 
-?>
+					echo '<tr><td align="left">' .
+					$row['PatientID'] . '</td><td align="left">' .
+					$row['Fname'] . '</td><td align="left">' .
+					$row['Mname'] . '</td><td align="left">' .
+					$row['Lname'] . '</td><td align="left">' .
+					$row['DOB'] . '</td><td align="left">' .
+					$row['Gender'] . '</td><td align="left">' .
+					$row['Sex'] . '</td><td align="left">' .
+					$row['Language'] . '</td><td align="left">' .
+					$row['Race'] . '</td><td align="left">'.
+					$row['Ethnicity'] . '</td><td align="left">';
+					echo '</tr>';
+				}
+
+				echo'</table>';
+			} else{
+				echo "Couldn't issue database query<br />";
+				echo mysqli_error($dbc);
+			}
+
+			mysqli_close($dbc);
+
+			?>
+		</div>
+
+
+		<script>
+			/* Set the width of the side navigation to 250px */
+			function openNav() {
+				document.getElementById("mySidenav").style.width = "250px";
+			}
+
+			/* Set the width of the side navigation to 0 */
+			function closeNav() {
+				document.getElementById("mySidenav").style.width = "0";
+			}
+		</script>
+	</body>
+	</html>
--- html/code/searchPatient.php
+++ html/code/searchPatient.php
@@ -9,7 +9,7 @@
 
 	<table class="patient-info-header">
 		<tr>
-			<td><img src="../img/menu.png" align="middle" href="index.html">
+			<td><img src="../img/menu.png" align="middle" href="index.html" onclick="openNav()"&#9776; >
 				CT# - <span id="ct-num"> 1234567890</span>
 			</td><td>
 				<h1 id="patient-name">Search Patient</h1>
@@ -20,65 +20,85 @@
 			</tr>
 		</table>
 
-		<div class="floating-box">
+		<div class="floating-box" id="centerTop">
 			<h3>Please enter one of the following:</h3>
-			<form method="post" action="http://localhost/searchPatient.php">
-				<label>Patient ID</label>
-				<input type="text" label="patientID" id="patientID">
-				<label>Patient Name</label>
-				<input type="text" label="patientName" id="patientName">
+			<form method="post" action="http://localhost/code/searchPatient.php">
+				<label>Patient ID:</label>
+				<input type="text" label="patientID" id="patientID" placeholder="000000">
+				<label>Patient Name:</label>
+				<input type="text" label="patientName" id="patientFName" placeholder="First Name">
+				<input type="text" label="patientName" id="patientLName" placeholder="Last Name">
 				<input type="submit" name="searchUser" value="Search"  id="searchUser">
 				<input type="submit" name="searchAll" value="Search All Patients"  id="searchAll">
 			</form>
 
 		</div>
 
-		<div class="floating-box">
-			<?php 
-			if(isset($_POST['searchAll'])){
-				require_once('connect.php');
-
-				$query = "SELECT Fname, Lname, DOB, Sex FROM patient";
-				$responce = @mysqli_query($dbc, $query);
-				$boxLimit = 4;
-				$count = 0;
-
-				if($responce){
-				
-					echo'<table>';
-					while($row = mysqli_fetch_array($responce)){
-						$count++;
-						if ($count > $boxLimit) {
-							echo'<tr>';
-						}
-						echo'
-							<td> 
-								<div class="floating-box" style="border: 1px solid black;">
-									 <b><h3>'. $row['Fname'] . '</h3></b><h5>'. $row['Lname'] . '</h5>
-									<li> DOB: '. $row['DOB'] . '</li>
-									<li> Sex: '. $row['Sex'] . '</li>
-									<li>Health Status: Alive</li>
-									<li>Last Visit: 365 days ago</li>
-									<li>Care Team: Team Deep</li>
-								</div>
-							</td>';
-
-						if ($count > $boxLimit){
-							echo'<?/tr>';
-							$count = 1;
-						}
-
-					}
-					echo'</table>';
-				} else{
-					echo "Couldn't issue database query<br />";
-					echo mysqli_error($dbc);
-				}
-
-				mysqli_close($dbc);
-			}
-			?>
+		<div id="mySidenav" class="sidenav">
+			<a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+			<a href="../login.html">Log In</a>
+			<a href="../index.html">Home</a>
+			<a href="getpatientinfo.php">View Database</a>
+			<a href="#">Contact</a>
 		</div>
 
-	</body>
-	</html>
+		<?php 
+		if(isset($_POST['searchAll'])){
+			require_once('connect.php');
+
+			$query = "SELECT Fname, Lname, DOB, Sex FROM patient";
+			$responce = @mysqli_query($dbc, $query);
+			$boxLimit = 4;
+			$count = 0;
+
+			if($responce){
+				
+				echo'<div class="floating-box">';
+				echo'<table>';
+				while($row = mysqli_fetch_array($responce)){
+					$count++;
+					if ($count > $boxLimit) {
+						echo'<tr>';
+					}
+					echo'
+					<td> 
+					<div class="floating-box" style="border: 1px solid black;" id="searchMe">
+					<b><h3>'. $row['Fname'] . '</h3></b><h5>'. $row['Lname'] . '</h5>
+					<li> DOB: '. $row['DOB'] . '</li>
+					<li> Sex: '. $row['Sex'] . '</li>
+					<li>Health Status: Alive</li>
+					<li>Last Visit: 365 days ago</li>
+					<li>Care Team: Team Deep</li>
+					</div>
+					</td>';
+
+					if ($count > $boxLimit){
+						echo'<?/tr>';
+						$count = 1;
+					}
+
+				}
+				echo'</table>';
+				echo'</div>';
+			} else{
+				echo "Couldn't issue database query<br />";
+				echo mysqli_error($dbc);
+			}
+
+			mysqli_close($dbc);
+		}
+		?>
+
+	<script>
+		/* Set the width of the side navigation to 250px */
+		function openNav() {
+			document.getElementById("mySidenav").style.width = "250px";
+		}
+
+		/* Set the width of the side navigation to 0 */
+		function closeNav() {
+			document.getElementById("mySidenav").style.width = "0";
+		}
+	</script>
+</body>
+</html>
