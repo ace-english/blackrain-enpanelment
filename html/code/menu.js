011a808188941c0c29aa2ea63ba677c7ffa8f30e
Ace English <Ace English>
2019-01-15 18:45:09 -0800

    adaptive menu now all html files

--- html/code/menu.js
+++ html/code/menu.js
@@ -1,19 +1,40 @@
 
 window.onload = function () { 
 
-	var headerHTML="<tr><td><img src='img/menu.png' align='middle' href='index.html' id='hamburger'>CT# - <span id='ct-num'> 1234567890</span></td><td style='width:60%;'></td><td><img src='img/bell.png' align='middle' >	USER ID	<span id='user-id'>_________</span>	</tr>";
-
-	document.getElementById("patient-info-header").innerHTML=headerHTML;
+	var prebodyHTML="<table id='patient-info-header'><tr><td><img src='img/menu.png' align='middle' href='index.html' id='hamburger' onclick='openNav()'>CT# - <span id='ct-num'> 1234567890</span></td><td style='width:60%;'></td><td><img src='img/bell.png' align='middle' >	USER ID	<span id='user-id'>_________</span>	</tr></table>";
 	
+	/**
+	*menu starts here
+	*/
 	
-	var menuHTML="<div class='side_menu'><ul><li><a href=''>« Hide</a></li><li><a href='login.html'>Log In</a></li><li><a href='addpatient.html'>Add Patient</a></li><li><a href='code/searchPatient.php'>Patient Search</a></li><li><a href='code/getpatientinfo.php'>View Database</a></li><li><a href=''>Page</a></li><li><a href=''>Page</a></li></ul></div>";
+	var menuLinks=[
+		{name:"Home",link:"index.html"},
+		{name:"Log In",link:"login.html"},
+		{name:"Add Patient",link:"addpatient.html"},
+		{name:"Search Patient", link:"code/searchPatient.php"},
+		{name:"View Database", link:"code/getpatientinfo.php"},
+		{name:"Analytics",link:"docpatient.html"},
+		{name:"Settings", link:"settings.html"}
+	];
 	
-	document.getElementById("side_menu_container").innerHTML=menuHTML;
+	prebodyHTML+="<div id='mySidenav' class='sidenav'><a href='javascript:void(0)' class='closebtn' onclick='closeNav()'>&times;</a>";
 	
-	$("#hamburger").click(function(){
-		$("#side_menu_container").toggle();
-	});
+	for(var i=0; i<menuLinks.length; i++){
+		prebodyHTML+="<a href='"+menuLinks[i].link+"'>"+menuLinks[i].name+"</a>";
+	}
+	prebodyHTML+="</div>";
+	var body=document.body.innerHTML;
+	document.body.innerHTML=prebodyHTML+body;
 	
 	
 	
 };
+/* Set the width of the side navigation to 250px */
+function openNav() {
+	document.getElementById("mySidenav").style.width = "250px";
+}
+
+			/* Set the width of the side navigation to 0 */
+function closeNav() {
+	document.getElementById("mySidenav").style.width = "0";
+}
