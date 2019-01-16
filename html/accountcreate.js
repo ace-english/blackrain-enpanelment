6b3113413884dbadcf394b84679cc430db232177
hushamharoun <shawn_723@hotmail.com>
2019-01-15 17:22:47 -0800

    Add files via upload

---
+++ html/accountcreate.js
@@ -0,0 +1,15 @@
+function create()
+    {
+        // var username database location
+        // var Password database location
+        var Fname = document.getElementById("Fname").value
+        var Lname = document.getElementById("Lname").value
+        var Mname = document.getElementById("Mname").value
+        var ProID = document.getElementById("ProID").value
+        var NPI = document.getElementById("NPI").value
+        var Designation = document.getElementByID("Designnation").value
+        var username = document.getElementById("userID").value; 
+        var password =document.getElementById("password").value;
+        
+        window.location = "index.html";
+    }
