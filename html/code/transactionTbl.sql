5a3519e40242c76bf2ebbd711061e56bd7a7a696
Jim Saetern <jimzo123@gmail.com>
2019-09-26 12:55:06 -0700

    request and panel tables added

--- html/code/transactionTbl.sql
+++ html/code/transactionTbl.sql
@@ -1,7 +1,7 @@
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
- SET NAMES utf8 ;
+ --SET NAMES utf8 ;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -9,25 +9,41 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-DROP TABLE IF EXISTS Patient;
+DROP TABLE IF EXISTS Request;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE Patient (
-  PatientID int NOT NULL AUTO_INCREMENT,
+ --SET character_set_client = utf8mb4 ;
+CREATE TABLE Request (
+  PatientID
+  FName varchar(155) NULL,
+  MName varchar(155) NULL,
+  LName varchar(155) NULL,
+  DOB date NULL,
+  MRN varchar(75) NULL,
+  PcpAssignmentID int NULL,
+  ProviderID int NULL,
+  ProviderName varchar(155) NULL,
+  VisitCnt int NULL,
+) ENGINE=InnoDB;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+DROP TABLE IF EXISTS Panel;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ --SET character_set_client = utf8mb4 ;
+CREATE TABLE Panel (
+  ProviderID int NULL,
   FName varchar(155) NULL,
   MName varchar(75) NULL,
   LName varchar(155) NULL,
-  DOB date NULL,
+  NPI varchar(15) NULL,
+  Taxonomy varchar(155) NULL,
+  ProDesignation varchar(155) NULL,
+  Available char(1) NULL,
   Gender varchar(75) NULL,
-  Sex char(2) NULL,
-  Language varchar(155) NULL,
-  Race varchar(155) NULL,
-  Ethnicity varchar(155) NULL,
-  HealthPlanID1 varchar(75) NULL,
-  HealthPlanID2 varchar(75) NULL,
-  SubscriberID1 varchar(75) NULL,
-  SubscriberID2 varchar(75) NULL,
-  MRN varchar(75) NULL,
-  CONSTRAINT PKPat PRIMARY KEY NONCLUSTERED (PatientID)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  DOB date NULL,
+  Title varchar(155) NULL,
+  Specialty1 varchar(155) NULL,
+  Specialty2 varchar(155) NULL,
+  Status char(1) NULL,
+  Size int NULL,
+) ENGINE=InnoDB;
+
