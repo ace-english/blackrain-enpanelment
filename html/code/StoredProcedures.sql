efb9af755fe7fc93821173b21af94a793243b9ad
Jim Saetern <jimzo123@gmail.com>
2019-09-27 20:51:58 -0700

    new searches added and update on transaction tables

---
+++ html/code/StoredProcedures.sql
@@ -0,0 +1,205 @@
+/* Procedure calls to fill in the rest of the Encounter,PcpAssignment, or Empanelment Table 
+    after inserting data into the DB */
+DELIMITER $$
+
+CREATE PROCEDURE EncounterFill()
+BEGIN
+UPDATE Encounter 
+SET Encounter.ProviderID = (
+    SELECT Provider.ProviderID 
+    FROM Provider
+    WHERE Provider.NPI = Encounter.NPI
+);
+
+UPDATE Encounter 
+SET Encounter.FacilityID = (
+    SELECT Facility.FacilityID 
+    FROM Facility
+    WHERE Facility.NPI = Encounter.NPI
+);
+
+UPDATE Encounter 
+SET Status = 'I';
+END$$
+
+DELIMITER ;
+
+DELIMITER $$
+
+CREATE PROCEDURE PcpAssignmentFill()
+BEGIN
+UPDATE PcpAssignment 
+SET PcpAssignment.FacilityID = (
+    SELECT Facility.FacilityID 
+    FROM Facility
+    WHERE Facility.NPI = PcpAssignment.NPI
+);
+
+UPDATE PcpAssignment 
+SET Status = 'I';
+END$$
+
+DELIMITER ;
+
+DELIMITER $$
+
+CREATE PROCEDURE EmpanelmentFill()
+BEGIN
+UPDATE Empanelment 
+SET Empanelment.PcpAssignmentID = (
+    SELECT PcpAssignment.PcpAssignmentID 
+    FROM PcpAssignment
+    WHERE PcpAssignment.NPI = Empanelment.NPI
+);
+
+UPDATE Empanelment 
+SET Empanelment.FacilityID = (
+    SELECT Facility.FacilityID 
+    FROM Facility
+    WHERE Facility.NPI = Empanelment.NPI
+);
+
+UPDATE Empanelment 
+SET Status = 'I';
+END$$
+
+DELIMITER ;
+
+/*Patient Searches for the front end*/
+DELIMITER $$
+
+CREATE PROCEDURE PatientSearch(IN MRN varchar(75))
+BEGIN
+    SELECT PatientID,FName,MName,LName,DOB,Gender,Sex,Language,Race,Ethnicity,Insurance1,HealthPlanID1,Insurance2,HealthPlanID2,SubscriberID1,SubscriberID2,MRN 
+    FROM Patient
+    WHERE Patient.MRN = MRN && Patient.Status != 'D';
+END$$
+
+DELIMITER ;
+
+DELIMITER $$
+
+CREATE PROCEDURE PatientSearch2(IN MRN varchar(75))
+BEGIN
+    SELECT FName, LName, DOB, MRN 
+    FROM Patient
+    WHERE Patient.MRN = MRN && Patient.Status != 'D'; 
+END$$
+
+DELIMITER ;
+
+DELIMITER $$
+
+CREATE PROCEDURE PatientSearchAlt(IN FName varchar(155), IN LName varchar(155), IN DOB date)
+BEGIN
+    SELECT PatientID,FName,MName,LName,DOB,Gender,Sex,Language,Race,Ethnicity,Insurance1,HealthPlanID1,Insurance2,HealthPlanID2,SubscriberID1,SubscriberID2,MRN 
+    FROM Patient
+    WHERE Patient.FName = FName && Patient.LName = LName && Patient.DOB = DOB && Patient.Status != 'D';
+END$$
+
+DELIMITER ;
+
+DELIMITER $$
+
+CREATE PROCEDURE PatientSearchAlt2(IN FName varchar(155), IN LName varchar(155), IN DOB date)
+BEGIN
+    SELECT FName, LName, DOB, MRN 
+    FROM Patient
+    WHERE Patient.FName = FName && Patient.LName = LName && Patient.DOB = DOB && Patient.Status != 'D';
+END$$
+
+DELIMITER ;
+
+/*Provider Searches for the front end*/
+DELIMITER $$
+
+CREATE PROCEDURE ProviderSearch(IN NPI varchar(15))
+BEGIN
+    SELECT ProviderID,FName,MName,LName,NPI,Taxonomy1,GenCode,Title,Size
+    FROM Panel
+    WHERE Panel.NPI = NPI && Panel.Status != 'D';
+END$$
+
+DELIMITER ;
+
+DELIMITER $$
+
+CREATE PROCEDURE ProviderSearch2(IN NPI varchar(15))
+BEGIN
+    SELECT NPI, FName, LName, Taxonomy1
+    FROM Panel
+    WHERE Panel.NPI = NPI && Panel.Status != 'D';
+END$$
+
+DELIMITER ;
+
+DELIMITER $$
+
+CREATE PROCEDURE ProviderSearchAlt(IN FName varchar(155),IN LName varchar(155),IN Taxonomy1 varchar(75))
+BEGIN
+    SELECT ProviderID,FName,MName,LName,NPI,Taxonomy1,GenCode,Title,Size
+    FROM Panel
+    WHERE Panel.FName = FName && Panel.LName = LName && Panel.Taxonomy1 = Taxonomy1 && Panel.Status != 'D';
+END$$
+
+DELIMITER ;
+
+DELIMITER $$
+
+CREATE PROCEDURE ProviderSearchAlt2(IN FName varchar(155),IN LName varchar(155),IN Taxonomy1 varchar(75))
+BEGIN
+    SELECT NPI, FName, LName, Taxonomy1
+    FROM Panel
+    WHERE Panel.FName = FName && Panel.LName = LName && Panel.Taxonomy1 = Taxonomy1 && Panel.Status != 'D';
+END$$
+
+DELIMITER ;
+
+/*Encounter Searches*/
+DELIMITER $$
+
+CREATE PROCEDURE EncounterSearch(IN PatientID int)
+BEGIN
+    SELECT COUNT(Encounter.NPI) AS VisitCnt, Provider.FName, Provider.LName, Encounter.NPI, Encounter.DOS, Encounter.EncID
+    FROM Encounter, Provider
+    WHERE Encounter.PatientID = PatientID && Encounter.NPI = Provider.NPI && Encounter.Status != 'D'
+    GROUP BY Provider.FName, Provider.LName, Encounter.NPI, Encounter.DOS, Encounter.EncID;
+END$$
+
+DELIMITER ;
+
+/*For adding and dropping master tables easier*/
+DELIMITER $$
+
+CREATE PROCEDURE DropAllTables()
+BEGIN
+    DROP TABLE IF EXISTS Empanelment;
+    DROP TABLE IF EXISTS PcpAssignment;
+    DROP TABLE IF EXISTS Encounter;
+    DROP TABLE IF EXISTS Users;
+    DROP TABLE IF EXISTS Facility;
+    DROP TABLE IF EXISTS Provider;
+    DROP TABLE IF EXISTS Patient;
+END$$
+
+DELIMITER ;
+
+DELIMITER $$
+
+CREATE PROCEDURE AddAllTables()
+BEGIN
+    CALL AddPatientTbl();
+    CALL AddProviderTbl();
+    CALL AddFacilityTbl();
+    CALL AddUsersTbl();
+    CALL AddEncounterTbl();
+    CALL AddPcpAssignmentTbl();
+    CALL AddEmpanelmentTbl();
+END$$
+
+DELIMITER ;
+
+/*For after inserting into Encounter,PcpAssignment,or Empanelment Tables*/
+--CALL EncounterFill();
+--CALL PcpAssignmentFill();
+--CALL EmpanelmentFill();
--- html/code/transactionTbl.sql
+++ html/code/transactionTbl.sql
@@ -1,45 +1,43 @@
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
- --SET NAMES utf8 ;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
+--Creates 
 DROP TABLE IF EXISTS Request;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- --SET character_set_client = utf8mb4 ;
+
 CREATE TABLE Request (
 SELECT Patient.PatientID, Patient.FName, Patient.MName, Patient.LName, Patient.DOB, Patient.MRN, Empanelment.PcpAssignmentID, Empanelment.ProviderID, Empanelment.ProviderName
 FROM Patient, Empanelment
 WHERE Patient.PatientID = Empanelment.PatientID
-) ENGINE=InnoDB;
-/*!40101 SET character_set_client = @saved_cs_client */;
+);
 
+/*CREATE PROCEDURE RequestAcceptted(IN ProName varchar())
+BEGIN
+    INSERT INTO Empanelment(TerminationDate)
+    VALUE();
+
+    INSERT INTO Empanelment(ProviderName, NPI, PatientID, PatientName, EffectiveDate, FacilityName, Insurance1,Insurance2, CurrentRecordFlag)
+    VALUE();
+    CALL EmpanelmentFill;
+END$$
+
+DELIMITER ;*/
+
+--Creates transaction table,add panelStatus field, and update panelStatus
 DROP TABLE IF EXISTS Panel;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- --SET character_set_client = utf8mb4 ;
-CREATE TABLE Panel (
-SELECT ProviderID, FName, MName, LName, NPI, Taxonomy, ProDesignation, Available, Gender, DOB, Title, Specialty1, Specialty2, Status, Size, panelStatus
-FROM 
-  ProviderID int NULL,
-  FName varchar(155) NULL,
-  MName varchar(75) NULL,
-  LName varchar(155) NULL,
-  NPI varchar(15) NULL,
-  Taxonomy varchar(155) NULL,
-  ProDesignation varchar(155) NULL,
-  Available char(1) NULL,
-  Gender varchar(75) NULL,
-  DOB date NULL,
-  Title varchar(155) NULL,
-  Specialty1 varchar(155) NULL,
-  Specialty2 varchar(155) NULL,
-  Status char(1) NULL,
-  Size int NULL,
-  panelStatus char(1) NULL
-) ENGINE=InnoDB;
 
+CREATE TABLE Panel (
+SELECT Provider.ProviderID, Provider.FName, Provider.MName, Provider.LName, Provider.NPI, Provider.Taxonomy1, Provider.GenCode, Provider.Title, Provider.Size, Provider.Status
+FROM Provider, Empanelment
+WHERE Provider.NPI = Empanelment.NPI
+);
+
+ALTER TABLE Panel
+  ADD COLUMN panelStatus char(1) NOT NULL,
+  ADD COLUMN exception1 varchar(155) NULL,
+  ADD COLUMN exception2 varchar(155) NULL, 
+  ADD COLUMN exception3 varchar(155) NULL,
+  ADD COLUMN exception4 varchar(155) NULL;
+
+--Updates panelstatus field by counting the number of patients by size
+UPDATE Panel 
+SET Panel.panelStatus = (
+    CASE
+
+);
