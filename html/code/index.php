f3f175b9d3d1f4ee05b3a7c8cb321f53b1353d61
Jim Saetern <jimzo123@gmail.com>
2019-10-15 14:26:04 -0700

    login page and first page of website

---
+++ html/code/index.php
@@ -0,0 +1,139 @@
+ <?php  
+ session_start();  
+ require_once 'dbconfig.php';  
+ try  
+ {  
+      $connect = new PDO("mysql:host=$host; dbname=$database", $username, $password);  
+      $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);  
+      if(isset($_POST["login"]))  
+      {  
+           if(empty($_POST["username"]) || empty($_POST["password"]))  
+           {  
+                $message = '<label>All fields are required</label>';  
+           }  
+           else  
+           {  
+                $query = "SELECT * FROM users WHERE username = :username AND password = :password";  
+                $statement = $connect->prepare($query);  
+                $statement->execute(  
+                  array(  
+                    'username' => $_POST["username"],  
+                    'password' => $_POST["password"]
+                  )  
+                );
+                $row = $statement->fetch(PDO::FETCH_BOTH);
+                $count = $statement->rowCount();  
+                if($count > 0 && $row['UserType'] == 'FO')  
+                {  
+                    $_SESSION["username"] = $_POST["username"];  
+                    header("location:FOHome.php");  
+                }
+                elseif($count > 0 && $row['UserType'] == 'BO')  
+                {  
+                    $_SESSION["username"] = $_POST["username"];  
+                    header("location:BOHome.php");  
+                }   
+                else  
+                {  
+                    $message = '<label>Wrong Data</label>';  
+                }  
+           }  
+      }  
+ }  
+ catch(PDOException $error)  
+ {  
+      $message = $error->getMessage();  
+ }  
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {font-family: Arial, Helvetica, sans-serif;}
+form {border: 3px solid #f1f1f1;}
+
+input[type=text], input[type=password] {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  box-sizing: border-box;
+}
+
+button {
+  background-color: #4CAF50;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  cursor: pointer;
+  width: 100%;
+}
+
+button:hover {
+  opacity: 0.8;
+}
+
+.cancelbtn {
+  width: auto;
+  padding: 10px 18px;
+  background-color: #f44336;
+}
+
+.imgcontainer {
+  text-align: center;
+  margin: 24px 0 12px 0;
+}
+
+.container {
+  padding: 16px;
+}
+
+span.psw {
+  float: right;
+  padding-top: 16px;
+}
+
+/* Change styles for span and cancel button on extra small screens */
+@media screen and (max-width: 300px) {
+  span.psw {
+     display: block;
+     float: none;
+  }
+  .cancelbtn {
+     width: 100%;
+  }
+}
+</style>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>  
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
+</head>
+<body>  
+<br />  
+<div class="container" ">  
+<?php  
+  if(isset($message))  
+  {  
+    echo '<label class="text-danger">'.$message.'</label>';  
+  } 
+?>  
+<h3 align="">LOGIN</h3><br />  
+                <form method="post">  
+                     <label>Username</label>  
+                     <input type="text" name="username" class="form-control" />  
+                     <br />  
+                     <label>Password</label>  
+                     <input type="password" name="password" class="form-control" />  
+                     <br />  
+                     <input type="submit" name="login" class="btn btn-info" value="Login" />  
+                </form>  
+           </div>
+           <div class="container" style="background-color:#f1f1f1">
+       <span class="psw">Forgot <a href="#">password?</a></span>
+  </div>  
+           <br />  
+      </body>  
+ </html>  
